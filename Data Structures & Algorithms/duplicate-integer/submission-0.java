8a7eb92e993e52266838e4d981e0f3c7eb59f54a
Suraj Kumar Nayak <119889764+surajkn1405@users.noreply.github.com>
2026-05-13 11:51:48 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
